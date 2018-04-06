abcd57f25f5e718caa0c3405ea5d83163cb2826d
Ivan Fateev <Ivan.Fateev@microsoft.com>
2018-04-06 18:07:52 +0300

    added postgres client to docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM odota/parser:latest
 
+RUN apt-get update
+RUN apt-get install --assume-yes postgresql-client
+
 ADD . .
 RUN chmod +x launch.sh
 
--- launch.sh
+++ launch.sh
@@ -6,4 +6,6 @@ wget -O replay.dem.bz2 $REPLAY_FILE
 echo "Extracting"
 bzip2 -d replay.dem.bz2
 curl localhost:5600 --data-binary "@replay.dem" > export.json
+MATCH_DATA=`cat export.json`
+psql -v match_data=$MATCH_DATA -c "INSERT INTO (match_id, match_data) VALUES ('$MATCH_ID', :match_data)"
 
