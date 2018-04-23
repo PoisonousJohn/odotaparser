0b8b7bd8a1bd47070272a456fc791af55c8c033a
Ivan Fateev <Ivan.Fateev@microsoft.com>
2018-04-23 16:36:09 +0300

    updated to the latest requirements

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM odota/parser:latest
 
 RUN apt-get update
-RUN apt-get install --assume-yes postgresql-client
-RUN apt-get install --assume-yes python-psycopg2
+RUN apt-get install --assume-yes aria2
 
 ADD launch.sh launch.sh
-ADD insert.py insert.py
 RUN chmod +x launch.sh
 
 CMD ["/bin/bash", "launch.sh"]
--- launch.sh
+++ launch.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 echo "Launching server"
 java -jar -Xmx150m /usr/src/parser/target/stats-0.1.0.jar 5600 &
-wget -O replay.dem.bz2 $REPLAY_FILE
+aria2c -j 10 -o replay.dem.bz2 $REPLAY_FILE
 echo "Extracting"
 bzip2 -d replay.dem.bz2
 curl localhost:5600 --data-binary "@replay.dem" > export.json
-python insert.py
-
+curl -X POST -H "Content-type: application/json" -d @export.json $URL
